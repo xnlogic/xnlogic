d1fd985a116f6194844658656cfb49997e853e46
Darrick Wiebe <dw@xnlogic.com>
2015-04-14 13:58:38 -0400

    Critical fix for server start script

--- lib/xnlogic/templates/vagrant/config/start.sh
+++ lib/xnlogic/templates/vagrant/config/start.sh
@@ -56,7 +56,8 @@ trap 'terminator' SIGINT
 
 export RELOAD=true
 echo "starting torquebox"
-lsof -i :8080 -sTCP:listen | grep . || torquebox run -J "-Xmx1g -XX:MaxPermSize=128m -Ddatomic.objectCacheMax=128m" &> /dev/null  &
+export JAVA_OPTS="-server -XX:+UseCompressedOops -Xmx2g -XX:MaxPermSize=256m -Ddatomic.objectCacheMax=64m"
+lsof -i :8080 -sTCP:listen | grep . || torquebox run &> /dev/null  &
 echo "Hit Ctrl+C to terminate"
 # Using cat to keep processes live
 cat
