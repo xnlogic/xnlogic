8cc96b43a378a7162f72c794517206e0d92c3423
Darrick Wiebe <dw@xnlogic.com>
2015-07-01 02:52:36 -0400

    Update diagnostics

--- lib/xnlogic/templates/vagrant/config/diagnostics.sh
+++ lib/xnlogic/templates/vagrant/config/diagnostics.sh
@@ -9,31 +9,28 @@ which tree || sudo apt-get install tree
 
 sudo netstat -lt
 sudo netstat -lt > $DIR/netstat.txt
+sudo docker ps > $DIR/docker_ps.txt
+sudo docker ps -a > $DIR/docker_ps_a.txt
+sudo docker images > $DIR/docker_images.txt
+sudo docker images -a > $DIR/docker_images_a.txt
 tree $XN_CLIENT > $DIR/tree-${XN_CLIENT}.txt
-tree $HOME/.m2 > $DIR/tree-m2.txt
 tree $HOME/.npm > $DIR/tree-npm.txt
 tree /etc/{init,init.d} > $DIR/tree-init.txt
-tree /opt/xn_apps > $DIR/tree-xn_apps.txt
 which ruby > $DIR/ruby-version.txt
 ruby -v >> $DIR/ruby-version.txt
-rvm list 2>&1 >> $DIR/ruby-version.txt
 
-cp -r $HOME/$XN_CLIENT/{config,script,Gemfile.lock,Jarfile.lock,${XN_CLIENT}.gemspec,Vagrantfile,config.ru,.gitignore} $DIR/$XN_CLIENT/lib
+cp -r $HOME/$XN_CLIENT/{config,script,Gemfile.lock,Jarfile.lock,${XN_CLIENT}.gemspec,Vagrantfile,config.ru,.gitignore,.dockerignore} $DIR/$XN_CLIENT
+cp .git/config $DIR/git_config.txt
 cp $HOME/$XN_CLIENT/lib/${XN_CLIENT}.rb $DIR/lib
 cp $HOME/{.bash_logout,.bash_profile,.bashrc,.curlrc,.gemrc,.gitconfig,.irb-history,.mkshrc,.profile,.zlogin,.zsh_history,.zshrc} $DIR/dotfiles
-sudo cp -r /etc/{datomic,mysql} $DIR/etc
-mkdir -p $DIR/etc/init
-sudo cp  /etc/init/{datomic,mysql}.conf $DIR/etc/init
 
-sudo cp /var/log/datomic.log $DIR/log
-mkdir -p $DIR/log/datomic
-sudo cp /opt/datomic/current/log/$(date +%Y-%m-%d).log $DIR/log/datomic
-cp $HOME/xn.dev/tmp/server.log $DIR/log
-sudo cp -r /var/log/mysql $DIR/log
+docker ps -a | tr -s ' ' | awk '{print $NF}' | tail -n +2 | xargs -n 1 -I X bash -c "docker logs -t X > $DIR/log/X 2> $DIR/log/X.2"
+docker ps -a | tr -s ' ' | awk '{print $NF}' | tail -n +2 | xargs -n 1 -I X bash -c "docker diff X > $DIR/log/X.diff"
 
 sudo chown -R $(whoami):$(whoami) $DIR
-cd $DIR
-tar -czvf $HOME/${XN_CLIENT}-diagnostics-${TS}.tgz .
+
+cd $HOME
+tar -czf $HOME/${XN_CLIENT}-diagnostics-${TS}.tgz diagnostics/$TS
 cd -
 
 rm -r $DIR
